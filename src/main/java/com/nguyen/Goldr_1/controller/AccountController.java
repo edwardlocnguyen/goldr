ad6db55bca2948960b2fdf6e993d8afe2c2f9dac
Edward Nguyen <edwardlocnguyen@gmail.com>
2023-02-13 13:43:07 -0600

    submit new account for works and redirects to users existing accounts table with loaded data

--- src/main/java/com/nguyen/Goldr_1/controller/AccountController.java
+++ src/main/java/com/nguyen/Goldr_1/controller/AccountController.java
@@ -5,26 +5,34 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nguyen.Goldr_1.model.Account;
+import com.nguyen.Goldr_1.model.Asset;
+import com.nguyen.Goldr_1.model.User;
+import com.nguyen.Goldr_1.repository.UserRepo;
 import com.nguyen.Goldr_1.services.AccountServices;
 
-@RestController
-//@Controller
+//@RestController
+@Controller
 @RequestMapping("/users/{userId}/accounts")
 public class AccountController {
 
 	@Autowired
 	private AccountServices accountServices;
+	@Autowired
+	private UserRepo userRepo;
 
 	@GetMapping()
 	public List<Account> getAccountsByUserId(@PathVariable("userId") Integer userId) {
@@ -35,11 +43,23 @@ public class AccountController {
 	public Optional<Account> getAccountById(@PathVariable("userId") Integer userId, @PathVariable("id") Integer id) {
 		return accountServices.findAccountByUserId(userId, id);
 	}
-
-	@PostMapping
-	public void createAccount(@PathVariable("userId") Integer userId, @RequestBody Account account) {
-		accountServices.addAccount(userId, account);
+	
+	@GetMapping("/form")
+	public String getAccountForm(Model model) {
+		model.addAttribute("account", new Account());
+		return "accountForm";
 	}
+
+	@PostMapping("/post")
+	public String createAccount(@RequestParam("userId") Integer userId, @ModelAttribute("account") Account account) {
+		accountServices.addAccount(userId, account);
+		return "redirect:/users/user-account/" + userId;
+	}
+
+//	@PostMapping
+//	public void createAccount(@PathVariable("userId") Integer userId, @RequestBody Account account) {
+//		accountServices.addAccount(userId, account);
+//	}
 
 	@PutMapping("/{id}")
 	public void updateAccount(@PathVariable("userId") Integer userId, @PathVariable("id") Integer id,
