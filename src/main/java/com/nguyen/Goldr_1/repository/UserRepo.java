41b5cec9a04079aaa221b6e4e30492c9bb70847f
Edward Nguyen <edwardlocnguyen@gmail.com>
2023-02-13 21:28:16 -0600

    loaded accounts-amounts data on bootstrap table

--- src/main/java/com/nguyen/Goldr_1/repository/UserRepo.java
+++ src/main/java/com/nguyen/Goldr_1/repository/UserRepo.java
@@ -12,12 +12,19 @@ import java.util.List;
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 
-	@Query(value = "SELECT a.id, a.name, SUM(t.amount) as total_amount FROM account a "
-			+ "INNER JOIN Txn t ON a.id = t.account_id "
-			+ "INNER JOIN (SELECT account_id, MAX(date) as max_date FROM Txn GROUP BY account_id) t1 "
-			+ "ON a.id = t1.account_id AND t.date = t1.max_date " + "WHERE a.user_id = :userId "
-			+ "GROUP BY a.id, a.name", nativeQuery = true)
+	@Query(value = "SELECT a.id, a.name AS account_name, asset.name AS asset_name, SUM(t.amount) AS total_amount "
+			+ "FROM account a " + "INNER JOIN Txn t ON a.id = t.account_id "
+			+ "INNER JOIN (SELECT account_id, MAX(date) AS max_date FROM Txn GROUP BY account_id) t1 "
+			+ "ON a.id = t1.account_id AND t.date = t1.max_date " + "INNER JOIN Asset asset ON t.asset_id = asset.id "
+			+ "WHERE a.user_id = :userId " + "GROUP BY a.id, a.name, asset.name", nativeQuery = true)
 	List<Object[]> findAccountsAmountsByUserId(@Param("userId") Integer userId);
+
+//	@Query(value = "SELECT a.id, a.name, SUM(t.amount) as total_amount FROM account a "
+//			+ "INNER JOIN Txn t ON a.id = t.account_id "
+//			+ "INNER JOIN (SELECT account_id, MAX(date) as max_date FROM Txn GROUP BY account_id) t1 "
+//			+ "ON a.id = t1.account_id AND t.date = t1.max_date " + "WHERE a.user_id = :userId "
+//			+ "GROUP BY a.id, a.name", nativeQuery = true)
+//	List<Object[]> findAccountsAmountsByUserId(@Param("userId") Integer userId);
 
 	@Query(value = "SELECT a.id, a.name, SUM(t.amount) as total_amount FROM asset a "
 			+ "INNER JOIN Txn t ON a.id = t.asset_id "
--- src/main/resources/static/scripts/userAccount.js
+++ src/main/resources/static/scripts/userAccount.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
     var userId = window.location.pathname.split("/")[2];
     $.get("/users/" + userId + "/api/accounts-amounts", function(data) {
-        var count = 1;
         for (var i = 0; i < data.length; i++) {
             var account = data[i];
-            var row = "<tr><td>" + count + "</td><td>" + account[1] + "</td><td>" + account[2] + "</td></tr>";
-            $("#accounts-table tbody").append(row);
-            count++;
+            var row = "<tr><td>" + (i + 1) + "</td><td>" + account[1] + "</td><td>" + account[2] + "</td><td>" + account[3] + "</td></tr>";
+            $("#accounts-table-body").append(row);
         }
     });
 });
